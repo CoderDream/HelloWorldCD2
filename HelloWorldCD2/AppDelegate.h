d68ce03f9cc9bff4a0891e12aa4c70c0f412863c
CoderDream <coderdream@gmail.com>
2012-06-24 12:33:37 +0800

    first commit

---
+++ HelloWorldCD2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorldCD2
+//
+//  Created by Coder Dream on 12-6-24.
+//  Copyright (c) 2012年 CoderDream's Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorldCD2/BookController.h
@@ -0,0 +1,13 @@
+//
+//  BookController.h
+//  HelloWorldCD2
+//
+//  Created by Coder Dream on 12-6-24.
+//  Copyright (c) 2012年 CoderDream's Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookController : UIWebView
+
+@end
