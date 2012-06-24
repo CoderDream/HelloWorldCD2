d452e7d33e1347b8bd523b049cde6171c3853024
CoderDream <coderdream@gmail.com>
2012-06-24 12:36:24 +0800

    add 2

---
+++ HelloWorldCD2/TestController.h
@@ -0,0 +1,13 @@
+//
+//  TestController.h
+//  HelloWorldCD2
+//
+//  Created by Coder Dream on 12-6-24.
+//  Copyright (c) 2012年 CoderDream's Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestController : UIWebView
+
+@end
